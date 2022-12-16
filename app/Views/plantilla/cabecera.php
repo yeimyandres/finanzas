e90189488b9f009de69ea61d30d0a4bc60cbd56a
Yeimy Arteaga <yeimyandres@gmail.com>
2022-12-16 15:44:27 -0500

    Ajustes navbar

--- app/Views/plantilla/cabecera.php
+++ app/Views/plantilla/cabecera.php
@@ -11,11 +11,11 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand">Finanzas Personales</a>
-        <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div id="my-nav" class="collapse navbar-collapse">
-            <ul class="navbar-nav mr-auto">
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
                 <li class="nav-item active">
                     <a class="nav-link" href="<?=base_url('listacuentas')?>">Cuentas</a>
                 </li>
